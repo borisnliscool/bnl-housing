0b5a2e9b669eac2eab822ce8354a94a0744b063c
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2022-05-27 00:20:47 +0200

    add vehicle limit option for shells (not required)

--- data/shells.lua
+++ data/shells.lua
@@ -4,5 +4,6 @@ return {
         spawn = 'shell_garages', 
         foot_entrance = vector4(-5.912841796875, -3.5457763671875, 0.50001907348632, 180.0),
         vehicle_entrance = vector4(0.146484375, 1.1177978515625, 0.50001907348632, 0.0),
+        vehicle_limit = 3,
     }
 }
