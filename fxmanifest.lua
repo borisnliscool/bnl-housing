f153aeb93ab8893140f028ebc01e066ab33611e5
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2023-05-26 20:18:56 +0200

    reset

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,47 +1,30 @@
-fx_version      'cerulean'
-lua54           'yes'
-game            'gta5'
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
 
-name            'bnl-housing'
-author          'Boris'
-version         '0.0.1'
-repository      'https://github.com/borisnliscool/bnl-housing'
-description     'Simple player property system'
+name 'bnl-housing'
+author 'Boris'
+version '0.0.1'
+repository 'https://github.com/borisnliscool/bnl-housing'
+description 'Simple player property system'
 
 dependencies {
-	'/onesync',
-	'ox_lib',
-	'oxmysql',
+    '/onesync',
+    'ox_lib',
+    'oxmysql',
 }
 
 shared_scripts {
-	'@ox_lib/init.lua',
-	'src/shared/sh_main.lua',
+    '@ox_lib/init.lua',
+    -- 'src/shared/sh_main.lua',
 }
 
 client_scripts {
-	'src/client/cl_main.lua',
-	'src/client/cl_functions.lua',
-	'src/client/cl_decoration.lua',
-	'src/client/blipmanager.lua',
+    'src/client/*.lua',
 }
 
 server_scripts {
-	'@oxmysql/lib/MySQL.lua',
-
-	'src/server/sv_main.lua',
-	'src/server/sv_functions.lua',
-	'src/server/sv_decoration.lua',
-	'src/server/sv_commands.lua',
-}
-
-ui_page 'src/client/html/index.html'
-
-files {
-	'src/client/html/index.html',
-	'src/client/html/main.js',
-	'src/client/html/audio/*.mp3',
-
-	'locales/*.json',
-	'data/*.lua',
+    '@oxmysql/lib/MySQL.lua',
+    'src/server/classes/*.lua',
+    'src/server/*.lua',
 }
