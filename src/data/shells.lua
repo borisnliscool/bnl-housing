bd16968065f07b8e3df241e2b59d9d839032410b
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2023-05-27 19:16:53 +0200

    add all shell foot entrances

--- src/data/shells.lua
+++ src/data/shells.lua
@@ -1,12 +1,176 @@
 Data = {}
 
 Data.Shells = {
+    shell_apartment1 = {
+        entrances = {
+            foot = vec3(-2.228882, 8.434662, 3.194336),
+        }
+    },
+    shell_apartment2 = {
+        entrances = {
+            foot = vec3(-2.228882, 8.434662, 3.194336),
+        }
+    },
+    shell_apartment3 = {
+        entrances = {
+            foot = vec3(11.432739, 4.386307, 1.993286),
+        }
+    },
+    shell_barber = {
+        entrances = {
+            foot = vec3(1.634888, 4.742355, -0.570923),
+        }
+    },
+    shell_coke1 = {
+        entrances = {
+            foot = vec3(-6.250854, 8.170929, -0.964233),
+        }
+    },
+    shell_coke2 = {
+        entrances = {
+            foot = vec3(-6.250854, 8.170929, -0.964233),
+        }
+    },
+    shell_frankaunt = {
+        entrances = {
+            foot = vec3(-0.250854, -5.371933, -0.572876),
+        }
+    },
+    shell_garagel = {
+        entrances = {
+            foot = vec3(12.105225, -14.273026, -1.013794),
+        }
+    },
+    shell_garages = {
+        entrances = {
+            foot = vec3(5.881104, 3.463226, -0.510254),
+        }
+    },
+    shell_garagem = {
+        entrances = {
+            foot = vec3(13.463501, 1.577515, -0.763794),
+        }
+    },
+    shell_gunstore = {
+        entrances = {
+            foot = vec3(-1.028809, -5.068634, -0.749390),
+        }
+    },
+    shell_highend = {
+        entrances = {
+            foot = vec3(-21.850830, -0.387314, 7.201294),
+        }
+    },
+    shell_highendv2 = {
+        entrances = {
+            foot = vec3(-9.877197, 0.852234, 1.923706),
+        }
+    },
+    shell_lester = {
+        entrances = {
+            foot = vec3(-1.674927, -5.635666, -0.373779),
+        }
+    },
+    shell_medium2 = {
+        entrances = {
+            foot = vec3(6.026123, 0.548950, -0.668457),
+        }
+    },
+    shell_medium3 = {
+        entrances = {
+            foot = vec3(5.459106, -1.640060, 1.200928),
+        }
+    },
+    shell_meth = {
+        entrances = {
+            foot = vec3(-6.250854, 8.170929, -0.964233),
+        }
+    },
+    shell_michael = {
+        entrances = {
+            foot = vec3(-8.914551, 5.559937, -4.064697)
+        }
+    },
+    shell_office1 = {
+        entrances = {
+            foot = vec3(1.212891, 4.795105, -0.726807),
+        }
+    },
+    shell_office2 = {
+        entrances = {
+            foot = vec3(3.375610, -2.035675, -0.885742),
+        }
+    },
+    shell_officebig = {
+        entrances = {
+            foot = vec3(-9.217896, -1.112595, -0.401489),
+        }
+    },
+    shell_ranch = {
+        entrances = {
+            foot = vec3(-0.751953, -5.437866, -1.428833),
+        }
+    },
+    shell_store1 = {
+        entrances = {
+            foot = vec3(-2.809082, -4.211502, -0.634888),
+        }
+    },
+    shell_store2 = {
+        entrances = {
+            foot = vec3(-0.804688, -4.686218, -1.169678),
+        }
+    },
+    shell_store3 = {
+        entrances = {
+            foot = vec3(-0.118896, -7.508194, -0.310181),
+        }
+    },
+    shell_trailer = {
+        entrances = {
+            foot = vec3(-1.437622, -1.890610, -0.493652),
+        }
+    },
+    shell_trevor = {
+        entrances = {
+            foot = vec3(0.157959, -3.525787, -0.417480),
+        }
+    },
+    shell_v16low = {
+        entrances = {
+            foot = vec3(4.667847, -6.189514, -1.655151),
+        }
+    },
+    shell_v16mid = {
+        entrances = {
+            foot = vec3(1.384399, -13.824677, -0.502563),
+        }
+    },
+    shell_warehouse1 = {
+        entrances = {
+            foot = vec3(-8.584839, 0.140152, -0.965454),
+        }
+    },
+    shell_warehouse2 = {
+        entrances = {
+            foot = vec3(-12.158447, 5.586304, -2.060913),
+        }
+    },
+    shell_warehouse3 = {
+        entrances = {
+            foot = vec3(2.109619, -1.692810, -0.943237),
+        }
+    },
     shell_weed = {
         entrances = {
             foot = vec3(17.326660, 11.667877, -2.111694),
-            vehicle = vec3(5.550842, -8.969482, -2.094849)
         }
-    }
+    },
+    shell_weed2 = {
+        entrances = {
+            foot = vec3(17.326660, 11.667877, -2.111694),
+        }
+    },
 }
 
 return Data.Shells
