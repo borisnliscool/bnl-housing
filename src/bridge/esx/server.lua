f04a33a0de0fa569eef494b9478c543c64b41d74
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2023-05-27 19:17:33 +0200

    onReady also on server

--- src/bridge/esx/server.lua
+++ src/bridge/esx/server.lua
@@ -1,5 +1,12 @@
 local ESX = exports['es_extended']:getSharedObject()
 
+function Bridge.onReady(cb)
+    CreateThread(function()
+        Wait(10)
+        cb()
+    end)
+end
+
 function Bridge.GetPlayerIdentifier(source)
     return ESX.GetPlayerFromId(source).getIdentifier()
 end
@@ -7,4 +14,3 @@ end
 function Bridge.GetPlayerName(source)
     return ESX.GetPlayerFromId(source).getName()
 end
-
