d8a3aed0aab64b777b11ed1561c87c1fc8c11796
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2023-05-28 22:43:28 +0200

    Update clientFunctions.lua

--- src/shared/clientFunctions.lua
+++ src/shared/clientFunctions.lua
@@ -1,30 +1,24 @@
--- todo
--- could make this work like: ClientFunctions.<name> = RegisterClientFunction
--- wouldn't have the logical event names anymore though
--- we would need to keep a count or something
-
 ClientFunctions = {}
+local functionCount = 0
 
-function RegisterClientFunction(name, func)
-    local eventName = ("%s:%s"):format(cache.resource, name)
+function RegisterClientFunction(func)
+    functionCount = functionCount + 1
+    local eventName = ("%s:%s"):format(cache.resource, functionCount)
 
     if IsDuplicityVersion() then
-        -- server
-        ClientFunctions[name] = function(source, ...)
+        return function(source, ...)
             TriggerClientEvent(eventName, source, ...)
         end
     else
-        -- client
         RegisterNetEvent(eventName, function(...)
             func(...)
         end)
-        ClientFunctions = nil
     end
 end
 
 CreateThread(function()
-    RegisterClientFunction("FadeIn", DoScreenFadeIn)
-    RegisterClientFunction("FadeOut", DoScreenFadeOut)
-    RegisterClientFunction("SetupInPropertyPoints", SetupInPropertyPoints)
-    RegisterClientFunction("RemoveInPropertyPoints", RemoveInPropertyPoints)
+    ClientFunctions.FadeIn = RegisterClientFunction(DoScreenFadeIn)
+    ClientFunctions.FadeOut = RegisterClientFunction(DoScreenFadeOut)
+    ClientFunctions.SetupInPropertyPoints = RegisterClientFunction(SetupInPropertyPoints)
+    ClientFunctions.RemoveInPropertyPoints = RegisterClientFunction(RemoveInPropertyPoints)
 end)
