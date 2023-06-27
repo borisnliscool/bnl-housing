eacd9834b2cdd43d5ce88365bfec9b5cc6a954e4
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2023-06-27 11:47:12 +0200

    add automatic framework detection & more annotations

--- config.lua
+++ config.lua
@@ -1,8 +1,14 @@
 Config = {}
 
----Framework you're using
----@type "esx"
-Config.framework = "esx"
+---@enum Framework
+FRAMEWORKS = {
+    auto = "auto",
+    esx = "esx",
+}
+---Framework you're using (automatic should be fine)
+---@type Framework
+Config.framework = FRAMEWORKS.auto
+
 ---The way for the menu to show up,
 ---@type "walk" | "keypress"
 Config.interactMode = "keypress"
@@ -16,23 +22,33 @@ Config.inviteExpire = 15
 ---@type number
 Config.inviteRange = 10.0
 
+---@class Keybind
+---@field padIndex number
+---@field control number
+---@field name string
+
+---@type table
 Config.Keybinds = {}
+---@type Keybind
 Config.Keybinds.invite = {
     padIndex = 0,
     control = 58,
     name = "INPUT_THROW_GRENADE"
 }
+---@type Keybind
 Config.Keybinds.exitGarage = {
     padIndex = 0,
     control = 87,
     name = "INPUT_VEH_FLY_THROTTLE_UP"
 }
+---@type Keybind
 Config.Keybinds.interact = {
     padIndex = 0,
     control = 38,
     name = "INPUT_PICKUP"
 }
 
+---@type table
 Config.VehicleBlacklist = {
     classes = {
         10, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21
@@ -47,9 +63,28 @@ Config.VehicleBlacklist = {
     }
 }
 
+---@type number
 Config.entranceTransition = 500
 
+---@class Point
+---@field viewDistance number
+---@field interact Keybind
+---@field marker Marker
+
+---@class Marker
+---@field type number
+---@field size vector3
+---@field vehicleSize number
+---@field offset vector3
+---@field rotation vector3
+---@field color table
+---@field bob boolean
+---@field faceCamera boolean
+
+---@type table
 Config.points = {}
+
+---@type Point
 Config.points.entrance = {
     viewDistance = 10.0,
     -- this is only used if Config.interactMode is set to "keypress"
@@ -73,9 +108,24 @@ Config.points.entrance = {
 -- the in-property point is set as the same
 -- as the entrance point, if you'd like to
 -- customize this you can do that
+---@type Point
 Config.points.property = Config.points.entrance
 
+---@class Blip
+---@field sprite number
+---@field color number
+---@field short boolean | nil
+---@field display number | nil
+---@field scale number | nil
+
+---@class PropertyBlips
+---@field owner Blip
+---@field member Blip
+---@field renter Blip
+
+---@type table
 Config.blips = {
+    ---@type PropertyBlips
     house = {
         owner = {
             sprite = 40,
@@ -91,6 +141,7 @@ Config.blips = {
             color = 5,
         },
     },
+    ---@type PropertyBlips
     warehouse = {
         owner = {
             sprite = 473,
@@ -106,6 +157,7 @@ Config.blips = {
             color = 5,
         },
     },
+    ---@type PropertyBlips
     office = {
         owner = {
             sprite = 475,
@@ -121,6 +173,7 @@ Config.blips = {
             color = 5,
         },
     },
+    ---@type PropertyBlips
     garage = {
         owner = {
             sprite = 357,
--- src/bridge/shared.lua
+++ src/bridge/shared.lua
@@ -1,11 +1,39 @@
 Bridge = {}
 
+---@return table
+local function GetAllResources()
+    local resources = {}
+
+    for i = 0, GetNumResources(), 1 do
+        local resourceName = GetResourceByFindIndex(i)
+        if resourceName and GetResourceState(resourceName) == "started" then
+            table.insert(resources, resourceName)
+        end
+    end
+
+    return resources
+end
+
+---@return Framework | nil
+local function DetectFramework()
+    local resources = GetAllResources()
+
+    if lib.table.contains(resources, "es_extended") then
+        return FRAMEWORKS.esx
+    end
+
+    Debug.Error("Unable to auto detect framework.")
+end
+
+---@type Framework | nil
+local framework = Config.framework ~= FRAMEWORKS.auto and Config.framework or DetectFramework()
+
 local version = IsDuplicityVersion() and "server" or "client"
-local script = ("src/bridge/%s/%s.lua"):format(Config.framework, version)
+local script = ("src/bridge/%s/%s.lua"):format(framework, version)
 local file = LoadResourceFile("bnl-housing", script)
 
 if not file then
-    return error(("Can't find bridge for framework '%s'"):format(Config.framework))
+    return error(("Can't find bridge for framework '%s'"):format(framework))
 end
 
 local func, err = load(file)
