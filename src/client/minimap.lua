31dd7732468f37e62c9d39bb51919566e9cff5fa
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2023-06-27 14:40:30 +0200

    Update minimap.lua

--- src/client/minimap.lua
+++ src/client/minimap.lua
@@ -3,14 +3,14 @@ local overlay = nil
 ---@param interior string
 ---@param location vector3
 ---@param zoom number
+---@todo This is unfinished, don't know if this will end up working
 function StartMinimapOverlay(interior, location, zoom)
     overlay = true
 
     CreateThread(function()
         while overlay do
             Wait(0)
-    
-            local coords = cache.coords
+
             SetRadarAsInteriorThisFrame(interior, 0, 0, cache.heading, zoom)
         end
     end)
