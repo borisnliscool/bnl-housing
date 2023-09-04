52e3c59ec3cb8771efbd80e12ef1c77f50c11232
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2023-09-04 17:59:48 +0200

    feat: getPropertiesVehicleIsIn export

--- src/server/exports.lua
+++ src/server/exports.lua
@@ -6,6 +6,26 @@ exports("getPropertyPlayerIsIn", function(playerId)
     return GetPropertyPlayerIsIn(playerId)
 end)
 
+exports("getPropertiesVehicleIsIn", function(vehiclePlate)
+    local propertyIds = {}
+
+    for id in pairs(Properties) do
+        local property = Properties[id] --[[@as Property]]
+
+        local vehicles = table.find(property.vehicles, function(vehicle)
+            return vehicle.props.plate == vehiclePlate
+        end)
+
+        if vehicles then
+            for _ = 1, #vehicles do
+                table.insert(propertyIds, property.id)
+            end
+        end
+    end
+
+    return propertyIds
+end)
+
 exports("warpPlayerIntoProperty", function(playerId, propertyId)
     return GetPropertyById(propertyId):enter(playerId)
 end)
