a7820d0dcd94cfa20724bbadf1e1f26827e50165
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2023-10-31 11:07:24 +0100

    feat: notification to player on rent paid

--- src/server/cronJobs.lua
+++ src/server/cronJobs.lua
@@ -1,13 +1,20 @@
 function StartRentCronJobs()
     for _, payment in pairs(DB.getPropertyRentPayments()) do
         lib.cron.new(payment.payment_interval, function()
-            Bridge.RemoveMoney(payment.player, payment.amount)
-
-            -- todo:
-            --  notification to the player if they are online
-
             local property = GetPropertyById(payment.property_id)
             if not property then return end
+
+            Bridge.RemoveMoney(payment.player, payment.amount)
+
+            if Bridge.GetServerIdFromIdentifier(payment.player) then
+                ClientFunctions.Notification(
+                    Bridge.GetServerIdFromIdentifier(payment.player),
+                    locale(
+                        "notification.rent.paid", payment.amount, property.address.streetName,
+                        property.address.buildingNumber
+                    )
+                )
+            end
 
             local ownerKey = table.findOne(property.keys, function(key)
                 return key.permission == PERMISSION.OWNER
